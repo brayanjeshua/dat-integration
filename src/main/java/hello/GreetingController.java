f7f189e9e47e355295d7641a330afc01440fc8b1
Anthony Dugarte <toonny1998@gmail.com>
2019-02-24 20:48:40 -0400

    Add basic authentication (not working yet)

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,19 +1,27 @@
 package hello;
 
+import java.rmi.RemoteException;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tcore.tcoreTypes.SessionToken;
+import com.transcore.connexion.sample.*;
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private final Login loginclient = new Login();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public SessionToken greeting(@RequestParam(value="email") String email,
+    		@RequestParam(value="password") String password) throws RemoteException {
+        
+    	return loginclient.log(email, password); 
+//        		Greeting(counter.incrementAndGet(),
+//                            String.format(template, name));
     }
 }
