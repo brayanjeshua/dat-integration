aa430e79540b7a007c50421a6e53e95688041864
Anthony Dugarte <toonny1998@gmail.com>
2019-03-01 09:58:37 -0400

    Add rest controller decorator to assets

--- src/main/java/hello/AssetController.java
+++ src/main/java/hello/AssetController.java
@@ -4,7 +4,9 @@ import java.rmi.RemoteException;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AssetController {
     @RequestMapping("/asset")
     public String asset(@RequestParam(value = "email") String email, @RequestParam(value = "password") String password,
