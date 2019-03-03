e6789fac947822fa2eceec996b88feabb1aaa7b5
Anthony Dugarte <toonny1998@gmail.com>
2019-03-03 19:36:43 -0400

    Post Shipment

--- src/main/java/hello/AssetController.java
+++ src/main/java/hello/AssetController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tcore.tcoreTypes.EquipmentType;
 import com.tcore.tcoreTypes.StateProvince;
 
 @RestController
 public class AssetController {
     @PostMapping(value = "/asset/shipment")
-    public Asset asset(@RequestParam(value = "email") String email, @RequestParam(value = "password") String password,
+    public AssetShipment assetShipment(@RequestParam(value = "email") String email,
+            @RequestParam(value = "password") String password,
+            @RequestParam(value = "equipmentType") String equipmentType,
+            @RequestParam(value = "lengthFeet") Integer lengthFeet,
+            @RequestParam(value = "weightPounds") Integer weightPounds,
             @RequestParam(value = "originCity") String originCity,
             @RequestParam(value = "originState") String originState,
             @RequestParam(value = "destinationCity") String destinationCity,
@@ -21,9 +26,10 @@ public class AssetController {
 
         final Login session = new Login(email, password);
 
-        System.out.println(StateProvince.Enum.forString(originState));
+        System.out.println(EquipmentType.Enum.forString(equipmentType));
 
-        return new Asset(session.getToken(), originCity, StateProvince.Enum.forString(originState), destinationCity,
+        return new AssetShipment(session.getToken(), EquipmentType.Enum.forString(equipmentType), lengthFeet,
+                weightPounds, originCity, StateProvince.Enum.forString(originState), destinationCity,
                 StateProvince.Enum.forString(destinationState), destinationLatitude, destinationLongitude);
     }
 }
