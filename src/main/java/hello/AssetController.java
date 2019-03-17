25aafc1ae8603488792d9c43949dcb0beb533777
Anthony Dugarte <toonny1998@gmail.com>
2019-03-17 16:20:26 -0400

    Clean unused endpoints

--- src/main/java/hello/AssetController.java
+++ src/main/java/hello/AssetController.java
@@ -8,29 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tcore.tcoreTypes.EquipmentType;
-import com.tcore.tcoreTypes.StateProvince;
-
 @RestController
 public class AssetController {
-    @PostMapping(value = "/asset/shipment")
-    public AssetShipment assetShipment(@RequestParam(value = "equipmentType") String equipmentType,
-            @RequestParam(value = "lengthFeet") Integer lengthFeet,
-            @RequestParam(value = "weightPounds") Integer weightPounds,
-            @RequestParam(value = "originCity") String originCity,
-            @RequestParam(value = "originState") String originState,
-            @RequestParam(value = "destinationCity") String destinationCity,
-            @RequestParam(value = "destinationState") String destinationState,
-            @RequestParam(value = "destinationLatitude") float destinationLatitude,
-            @RequestParam(value = "destinationLongitude") float destinationLongitude) throws RemoteException {
-
-        System.out.println(EquipmentType.Enum.forString(equipmentType));
-
-        return new AssetShipment(Login.sessionToken, EquipmentType.Enum.forString(equipmentType), lengthFeet,
-                weightPounds, originCity, StateProvince.Enum.forString(originState), destinationCity,
-                StateProvince.Enum.forString(destinationState), destinationLatitude, destinationLongitude);
-    }
-
     @PostMapping(value = "/asset")
     public String postAsset(@RequestParam(value = "assetType") String assetType,
             @RequestParam(value = "equipmentType") String equipmentType,
