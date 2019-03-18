cce143824a375382bb8368fe0edf883509938122
Anthony Dugarte <toonny1998@gmail.com>
2019-03-17 22:00:06 -0400

    Clean merge conflicts

--- src/main/java/hello/AssetController.java
+++ src/main/java/hello/AssetController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tcore.tcoreTypes.EquipmentType;
-import com.tcore.tcoreTypes.StateProvince;
 import com.tcore.tfmiFreightMatching.CreateSearchSuccessData;
 
 class User {
@@ -25,24 +23,6 @@ public class AssetController {
     public CreateSearchSuccessData assetShipmentGet(@RequestBody User user) throws RemoteException {
         return new Search().shipmentSearchPostalCode2CityState(Login.sessionToken);
         // return user;
-    }
-
-    @PostMapping(value = "/asset/shipment")
-    public AssetShipment assetShipment(@RequestParam(value = "equipmentType") String equipmentType,
-            @RequestParam(value = "lengthFeet") Integer lengthFeet,
-            @RequestParam(value = "weightPounds") Integer weightPounds,
-            @RequestParam(value = "originCity") String originCity,
-            @RequestParam(value = "originState") String originState,
-            @RequestParam(value = "destinationCity") String destinationCity,
-            @RequestParam(value = "destinationState") String destinationState,
-            @RequestParam(value = "destinationLatitude") float destinationLatitude,
-            @RequestParam(value = "destinationLongitude") float destinationLongitude) throws RemoteException {
-
-        System.out.println(EquipmentType.Enum.forString(equipmentType));
-
-        return new AssetShipment(Login.sessionToken, EquipmentType.Enum.forString(equipmentType), lengthFeet,
-                weightPounds, originCity, StateProvince.Enum.forString(originState), destinationCity,
-                StateProvince.Enum.forString(destinationState), destinationLatitude, destinationLongitude);
     }
 
     @PostMapping(value = "/asset")
